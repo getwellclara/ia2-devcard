dce37985d10df67219fa65e93b6c4815933ceaaf
clara <clarafreitasm27@gmail.com>
2026-05-14 00:11:24 -0300

    inicialização da estrutura base

---
+++ src/app/sucesso.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function SucessoScreen(){
+    return(
+       <SafeAreaView>
+            <View>
+                <Text>Sucesso Screen</Text>
+            </View>
+       </SafeAreaView>
+    )
+}
